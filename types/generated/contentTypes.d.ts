372f4a13ea808c12003535710782b04b56efa0bb
RESETOR205 <igorbibor@icloud.com>
2026-07-24 19:13:42 +0300

    Добавлена верификация по 6-значному коду

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -931,7 +931,6 @@ export interface PluginUsersPermissionsUser
   };
   options: {
     draftAndPublish: false;
-    timestamps: true;
   };
   attributes: {
     blocked: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
@@ -972,6 +971,7 @@ export interface PluginUsersPermissionsUser
       Schema.Attribute.SetMinMaxLength<{
         minLength: 3;
       }>;
+    verificationCode: Schema.Attribute.String;
   };
 }
 
