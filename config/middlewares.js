74c617c41813e1a449813c7e7c0dc839b57deaa2
RESETOR205 <igorbibor@icloud.com>
2026-08-01 19:00:39 +0300

    Fix URL syntax in CORS

--- config/middlewares.js
+++ config/middlewares.js
@@ -18,7 +18,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5500', '[http://127.0.0.1:5500](http://127.0.0.1:5500)', '[https://resetor205.github.io](https://resetor205.github.io)'],
+      origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'https://resetor205.github.io'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
