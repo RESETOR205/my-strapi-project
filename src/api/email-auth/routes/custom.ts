1c33335cc88b3b3a78b8dfa44182e89688b7cab8
RESETOR205 <igorbibor@icloud.com>
2026-08-02 16:07:40 +0300

    Исправлена логика авторизации: добавлен identifier и e.preventDefault()

--- src/api/email-auth/routes/custom.ts
+++ src/api/email-auth/routes/custom.ts
@@ -3,13 +3,13 @@ export default {
     {
       method: 'POST',
       path: '/email-auth/send',
-      handler: 'api::email-auth.email-auth.sendCode', // <-- вернул правильную приставку api::
+      handler: 'api::email-auth.email-auth.sendCode',
       config: { auth: false }
     },
     {
       method: 'POST',
       path: '/email-auth/verify',
-      handler: 'api::email-auth.email-auth.verifyCode', // <-- вернул правильную приставку api::
+      handler: 'api::email-auth.email-auth.verifyCode',
       config: { auth: false }
     }
   ]
